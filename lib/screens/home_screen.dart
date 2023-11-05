7ad948e7b86a203098d709cdb9fbb3f6c5103bc1
OnkarSagare27 <onkardsagare@gmail.com>
2023-11-05 23:57:18 +0530

    License

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,147 +1,189 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  var _selectedPageIndex;
+  final List<Widget> _pages = [
+    Container(
+      child: Center(
+        child: Text('Home'),
+      ),
+    ),
+    Container(
+      child: Center(
+        child: Text('Home'),
+      ),
+    ),
+    Container(
+      child: Center(
+        child: Text('Home'),
+      ),
+    ),
+    Container(
+      child: Center(
+        child: Text('Home'),
+      ),
+    ),
+    Container(
+      child: Center(
+        child: Text('Home'),
+      ),
+    ),
+  ];
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _selectedPageIndex = 0;
+
+    _pageController = PageController(initialPage: _selectedPageIndex);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+
+    super.dispose();
+  }
+
+  int currentPageIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 6,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          title: SizedBox(
-            width: 100,
-            child: Image.asset('assets/yt_symbol_text_logo.png'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: SizedBox(
+          width: 100,
+          child: Image.asset('assets/yt_symbol_text_logo.png'),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () => print('Pressed Cast'),
+            icon: Icon(Icons.cast_sharp),
           ),
-          actions: [
-            IconButton(
-              onPressed: () => print('Pressed Cast'),
-              icon: Icon(Icons.cast_sharp),
-            ),
-            IconButton(
-              onPressed: () => print('Pressed Notifications'),
-              icon: Icon(Icons.notifications_outlined),
-            ),
-            IconButton(
-              onPressed: () => print('Pressed Search'),
-              icon: Icon(Icons.search_rounded),
-            ),
-          ],
-          bottom: PreferredSize(
-            preferredSize: const Size.fromHeight(50),
-            child: TabBar(
-              labelPadding: EdgeInsets.all(5),
-              isScrollable: true,
-              unselectedLabelColor: Colors.white.withOpacity(0.3),
-              indicatorColor: Colors.black,
-              tabs: [
-                Tab(
-                  child: Container(
-                    padding: const EdgeInsets.only(
-                        top: 5, left: 10, right: 10, bottom: 5),
-                    margin: EdgeInsets.only(right: 5, left: 5),
-                    decoration: const BoxDecoration(
-                        color: Color(0xFF272727),
-                        borderRadius: BorderRadius.all(Radius.circular(5))),
-                    child: const Icon(
-                      Icons.explore_outlined,
-                      color: Colors.white,
-                    ),
+          IconButton(
+            onPressed: () => print('Pressed Notifications'),
+            icon: Icon(Icons.notifications_outlined),
+          ),
+          IconButton(
+            onPressed: () => print('Pressed Search'),
+            icon: Icon(Icons.search_rounded),
+          ),
+        ],
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(50),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(
+                      top: 5, left: 10, right: 10, bottom: 5),
+                  margin: EdgeInsets.only(right: 15, left: 15),
+                  decoration: const BoxDecoration(
+                      color: Color(0xFF272727),
+                      borderRadius: BorderRadius.all(Radius.circular(5))),
+                  child: const Icon(
+                    Icons.explore_outlined,
+                    color: Colors.white,
                   ),
                 ),
-                Tab(
-                  child: Container(
-                      width: 50,
-                      height: 35,
-                      padding: const EdgeInsets.only(
-                          top: 5, left: 10, right: 10, bottom: 5),
-                      decoration: const BoxDecoration(
+                Container(
+                    width: 50,
+                    height: 35,
+                    margin: EdgeInsets.only(right: 10),
+                    padding: const EdgeInsets.only(
+                        top: 5, left: 10, right: 10, bottom: 5),
+                    decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                    child: const Center(
+                      child: Text(
+                        'All',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    )),
+                Container(
+                  height: 35,
+                  margin: EdgeInsets.only(right: 10),
+                  padding: const EdgeInsets.only(
+                      top: 5, left: 10, right: 10, bottom: 5),
+                  decoration: const BoxDecoration(
+                      color: Color(0xFF272727),
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  child: const Center(
+                    child: Text(
+                      'Computer Programming',
+                      style: TextStyle(
                           color: Colors.white,
-                          borderRadius: BorderRadius.all(Radius.circular(10))),
-                      child: const Center(
-                        child: Text(
-                          'All',
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 15,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      )),
-                ),
-                Tab(
-                  child: Container(
-                    height: 35,
-                    padding: const EdgeInsets.only(
-                        top: 5, left: 10, right: 10, bottom: 5),
-                    decoration: const BoxDecoration(
-                        color: Color(0xFF272727),
-                        borderRadius: BorderRadius.all(Radius.circular(10))),
-                    child: const Center(
-                      child: Text(
-                        'Computer Programming',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold),
-                      ),
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold),
                     ),
                   ),
                 ),
-                Tab(
-                  child: Container(
-                    height: 35,
-                    padding: const EdgeInsets.only(
-                        top: 5, left: 10, right: 10, bottom: 5),
-                    decoration: const BoxDecoration(
-                        color: Color(0xFF272727),
-                        borderRadius: BorderRadius.all(Radius.circular(10))),
-                    child: const Center(
-                      child: Text(
-                        'Music',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold),
-                      ),
+                Container(
+                  height: 35,
+                  margin: EdgeInsets.only(right: 10),
+                  padding: const EdgeInsets.only(
+                      top: 5, left: 10, right: 10, bottom: 5),
+                  decoration: const BoxDecoration(
+                      color: Color(0xFF272727),
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  child: const Center(
+                    child: Text(
+                      'Music',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold),
                     ),
                   ),
                 ),
-                Tab(
-                  child: Container(
-                    height: 35,
-                    padding: const EdgeInsets.only(
-                        top: 5, left: 10, right: 10, bottom: 5),
-                    decoration: const BoxDecoration(
-                        color: Color(0xFF272727),
-                        borderRadius: BorderRadius.all(Radius.circular(10))),
-                    child: const Center(
-                      child: Text(
-                        'Live',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold),
-                      ),
+                Container(
+                  height: 35,
+                  margin: EdgeInsets.only(right: 10),
+                  padding: const EdgeInsets.only(
+                      top: 5, left: 10, right: 10, bottom: 5),
+                  decoration: const BoxDecoration(
+                      color: Color(0xFF272727),
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  child: const Center(
+                    child: Text(
+                      'Live',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold),
                     ),
                   ),
                 ),
-                Tab(
-                  child: Container(
-                    height: 35,
-                    padding: const EdgeInsets.only(
-                        top: 5, left: 10, right: 10, bottom: 5),
-                    decoration: const BoxDecoration(
-                        color: Color(0xFF272727),
-                        borderRadius: BorderRadius.all(Radius.circular(10))),
-                    child: const Center(
-                      child: Text(
-                        'Coding',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold),
-                      ),
+                Container(
+                  height: 35,
+                  margin: EdgeInsets.only(right: 10),
+                  padding: const EdgeInsets.only(
+                      top: 5, left: 10, right: 10, bottom: 5),
+                  decoration: const BoxDecoration(
+                      color: Color(0xFF272727),
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  child: const Center(
+                    child: Text(
+                      'Coding',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold),
                     ),
                   ),
                 ),
@@ -149,16 +191,48 @@ class HomeScreen extends StatelessWidget {
             ),
           ),
         ),
-        body: TabBarView(
-          children: <Widget>[
-            Container(child: Center(child: Text("Tab 1"))),
-            Container(child: Center(child: Text("Tab 2"))),
-            Container(child: Center(child: Text("Tab 3"))),
-            Container(child: Center(child: Text("Tab 4"))),
-            Container(child: Center(child: Text("Tab 5"))),
-            Container(child: Center(child: Text("Tab 6"))),
-          ],
-        ),
+      ),
+      body: PageView(
+        controller: _pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: _pages,
+      ),
+      bottomNavigationBar: NavigationBar(
+        backgroundColor: Colors.white,
+        onDestinationSelected: (selectedPageIndex) {
+          setState(() {
+            _selectedPageIndex = selectedPageIndex;
+            _pageController.jumpToPage(selectedPageIndex);
+          });
+        },
+        indicatorColor: Colors.orange,
+        selectedIndex: _selectedPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+            tooltip: 'Home',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.map_rounded),
+            icon: Icon(Icons.map_outlined),
+            label: 'Bases',
+            tooltip: 'Bases',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.search_rounded),
+            icon: Icon(Icons.search_outlined),
+            label: 'Search',
+            tooltip: 'Search',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.settings_rounded),
+            icon: Icon(Icons.settings_outlined),
+            label: 'Settings',
+            tooltip: 'Settings',
+          ),
+        ],
       ),
     );
   }

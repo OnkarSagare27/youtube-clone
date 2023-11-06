87c919c8bf1b6a8b674be5b2828a9a2db581510f
OnkarSagare27 <onkardsagare@gmail.com>
2023-11-06 23:59:05 +0530

    Home Page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_clone/screens/pages/home_page.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,11 +11,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   var _selectedPageIndex;
   final List<Widget> _pages = [
-    Container(
-      child: Center(
-        child: Text('Home'),
-      ),
-    ),
+    HomePage(),
     Container(
       child: Center(
         child: Text('Shorts'),
@@ -199,166 +196,177 @@ class _HomeScreenState extends State<HomeScreen> {
         physics: const NeverScrollableScrollPhysics(),
         children: _pages,
       ),
-      bottomNavigationBar: NavigationBar(
-        height: 45,
-        labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
-        backgroundColor: Color(0xFF0F0F0F),
-        onDestinationSelected: (selectedPageIndex) {
-          setState(() {
-            _selectedPageIndex = selectedPageIndex;
-            _pageController.jumpToPage(selectedPageIndex);
-          });
-        },
-        indicatorColor: Colors.transparent,
-        selectedIndex: _selectedPageIndex,
-        destinations: const <Widget>[
-          NavigationDestination(
-            selectedIcon: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.home_filled,
-                  color: Colors.white,
+      bottomNavigationBar: Container(
+        height: 47,
+        child: Column(
+          children: [
+            Container(
+              height: 2,
+              color: Color(0xFF2B2B2B),
+            ),
+            NavigationBar(
+              height: 45,
+              labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
+              backgroundColor: Color(0xFF0F0F0F),
+              onDestinationSelected: (selectedPageIndex) {
+                setState(() {
+                  _selectedPageIndex = selectedPageIndex;
+                  _pageController.jumpToPage(selectedPageIndex);
+                });
+              },
+              indicatorColor: Colors.transparent,
+              selectedIndex: _selectedPageIndex,
+              destinations: const <Widget>[
+                NavigationDestination(
+                  selectedIcon: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.home_filled,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        'Home',
+                        style: TextStyle(color: Colors.white, fontSize: 12),
+                      )
+                    ],
+                  ),
+                  icon: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Column(
+                        children: [
+                          Icon(
+                            Icons.home_filled,
+                            color: Colors.white,
+                          ),
+                          Text(
+                            'Home',
+                            style: TextStyle(color: Colors.white, fontSize: 12),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                  label: 'Home',
+                  tooltip: 'Home',
                 ),
-                Text(
-                  'Home',
-                  style: TextStyle(color: Colors.white, fontSize: 12),
-                )
+                NavigationDestination(
+                  selectedIcon: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.video_call,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        'Shorts',
+                        style: TextStyle(color: Colors.white, fontSize: 12),
+                      )
+                    ],
+                  ),
+                  icon: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.video_call_outlined,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        'Shorts',
+                        style: TextStyle(color: Colors.white, fontSize: 12),
+                      )
+                    ],
+                  ),
+                  label: 'Shorts',
+                  tooltip: 'Shorts',
+                ),
+                NavigationDestination(
+                  selectedIcon: Icon(
+                    Icons.add_circle_outline,
+                    size: 40,
+                    color: Colors.white,
+                  ),
+                  icon: Icon(
+                    Icons.add_circle_outline,
+                    size: 40,
+                    color: Colors.white,
+                  ),
+                  label: 'Upload',
+                  tooltip: 'Upload',
+                ),
+                NavigationDestination(
+                  selectedIcon: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.subscriptions,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        'Subscriptions',
+                        style: TextStyle(color: Colors.white, fontSize: 12),
+                      )
+                    ],
+                  ),
+                  icon: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.subscriptions_outlined,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        'Subscriptions',
+                        style: TextStyle(color: Colors.white, fontSize: 12),
+                      )
+                    ],
+                  ),
+                  label: 'Home',
+                  tooltip: 'Home',
+                ),
+                NavigationDestination(
+                  selectedIcon: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.account_circle,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        'You',
+                        style: TextStyle(color: Colors.white),
+                      )
+                    ],
+                  ),
+                  icon: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.account_circle_outlined,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        'You',
+                        style: TextStyle(color: Colors.white),
+                      )
+                    ],
+                  ),
+                  label: 'Account',
+                  tooltip: 'Account',
+                ),
               ],
             ),
-            icon: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Column(
-                  children: [
-                    Icon(
-                      Icons.home_filled,
-                      color: Colors.white,
-                    ),
-                    Text(
-                      'Home',
-                      style: TextStyle(color: Colors.white, fontSize: 12),
-                    )
-                  ],
-                ),
-              ],
-            ),
-            label: 'Home',
-            tooltip: 'Home',
-          ),
-          NavigationDestination(
-            selectedIcon: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.video_call,
-                  color: Colors.white,
-                ),
-                Text(
-                  'Shorts',
-                  style: TextStyle(color: Colors.white, fontSize: 12),
-                )
-              ],
-            ),
-            icon: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.video_call_outlined,
-                  color: Colors.white,
-                ),
-                Text(
-                  'Shorts',
-                  style: TextStyle(color: Colors.white, fontSize: 12),
-                )
-              ],
-            ),
-            label: 'Shorts',
-            tooltip: 'Shorts',
-          ),
-          NavigationDestination(
-            selectedIcon: Icon(
-              Icons.add_circle_outline,
-              size: 40,
-              color: Colors.white,
-            ),
-            icon: Icon(
-              Icons.add_circle_outline,
-              size: 40,
-              color: Colors.white,
-            ),
-            label: 'Upload',
-            tooltip: 'Upload',
-          ),
-          NavigationDestination(
-            selectedIcon: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.subscriptions,
-                  color: Colors.white,
-                ),
-                Text(
-                  'Subscriptions',
-                  style: TextStyle(color: Colors.white, fontSize: 12),
-                )
-              ],
-            ),
-            icon: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.subscriptions_outlined,
-                  color: Colors.white,
-                ),
-                Text(
-                  'Subscriptions',
-                  style: TextStyle(color: Colors.white, fontSize: 12),
-                )
-              ],
-            ),
-            label: 'Home',
-            tooltip: 'Home',
-          ),
-          NavigationDestination(
-            selectedIcon: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.account_circle,
-                  color: Colors.white,
-                ),
-                Text(
-                  'You',
-                  style: TextStyle(color: Colors.white),
-                )
-              ],
-            ),
-            icon: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.account_circle_outlined,
-                  color: Colors.white,
-                ),
-                Text(
-                  'You',
-                  style: TextStyle(color: Colors.white),
-                )
-              ],
-            ),
-            label: 'Account',
-            tooltip: 'Account',
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/video_card.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class VideoCard extends StatelessWidget {
+  const VideoCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(children: [Image.asset('assets/thumbnails/valorant.jpg')]),
+    );
+  }
+}
